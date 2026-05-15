1b55cf49188c8ed199580bf8d4e1b12f1fd256d2
jennyHome <lj_1124@126.com>
2026-05-15 21:44:10 +0800

    hhh

--- day01/arrAdd.c
+++ day01/arrAdd.c
@@ -2,7 +2,7 @@
 
 
 int main(){
-    int arr[] = {1,2,34,45,56,67,87};
+    int arr[] = {1,2,34,45,56,67,87,22};
     // for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
     //     // printf("result=%d\n",arr[i]);
     //     printf("arr[%d]=%d\n",i,*(arr+i));
